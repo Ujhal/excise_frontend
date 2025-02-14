2b5770386c4a4edea4e0108d46b40c6c3359d054
Tushar Rai <tusharrai818@gmail.com>
2025-02-14 16:29:45 +0530

    add step components structure for stepper

---
+++ src/app/licensee/licensee-dashboard/steps/step4/step4.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step4',
+  imports: [],
+  templateUrl: './step4.component.html',
+  styleUrl: './step4.component.scss'
+})
+export class Step4Component {
+
+}
