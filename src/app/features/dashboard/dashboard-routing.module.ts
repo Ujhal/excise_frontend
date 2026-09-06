927cb1c7bb89eeb4a9df8e4c85adb4e2912b5125
ITSALREADYBROKEN <sameerkhan432108765@gmail.com>
2026-09-06 19:17:08 +0530

    design updated

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -329,6 +329,20 @@ const routes: Routes = [
         data: { authorities: ['site_admin'], aboutUsCategory: 'aboutUsText' }
       },
       {
+        path: 'admin/about-us/products-and-services',
+        loadComponent: () =>
+          import('../admin/master/about-us/list/list.component').then((m) => m.ListComponent),
+        canActivate: [UserRouteAccessService],
+        data: { authorities: ['site_admin'], aboutUsCategory: 'productsAndServices' }
+      },
+      {
+        path: 'admin/about-us/refund-and-cancellation-policy',
+        loadComponent: () =>
+          import('../admin/master/about-us/list/list.component').then((m) => m.ListComponent),
+        canActivate: [UserRouteAccessService],
+        data: { authorities: ['site_admin'], aboutUsCategory: 'refundCancellationPolicy' }
+      },
+      {
         path: 'admin/preventive-raids',
         loadComponent: () =>
           import('../admin/master/preventive-raids/list/list.component').then((m) => m.ListComponent),
